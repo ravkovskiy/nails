c2275f7f0bd60ed53cccd3b0d42679249fc2c671
ravkovskiy <kos1992@mail.ru>
2016-04-20 10:28:33 +0300

    add additionally carousel

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from 'angular2/core';
 import {TemplateHeader} from './template/template-header';
 import {TemplateSlider} from './template/template-slider';
+import {TemplateCarousel} from './template/template-carousel';
 
 @Component({
     selector: 'my-app',
-    directives: [TemplateHeader, TemplateSlider],
+    directives: [TemplateHeader, TemplateSlider,TemplateCarousel],
     template: `
         <template-header></template-header>
         <template-slider></template-slider>
+        <template-carousel></template-carousel>
     `
 })
 export class AppComponent { }
