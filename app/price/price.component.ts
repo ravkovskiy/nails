4028beceb98445810017076f532e3442784392ff
ravkovskiy <kos1992@mail.ru>
2016-04-28 17:22:43 +0300

    add price-service and using ng-for for price-service

--- app/price/price.component.ts
+++ app/price/price.component.ts
@@ -1,10 +1,18 @@
 import {Component} from "angular2/core";
+import {PriceService} from "../price/price.service";
 @Component({
     selector: 'price_component',
     template: `
         <div class='price'>
             <span>Price</span>
+            <li *ngFor="#price of priceService.price">
+                {{price}}
+            </li>
         </div>
     `
 })
-export class PriceComponent{}
+export class PriceComponent{
+    constructor(public priceService:PriceService) {
+     
+    }
+}
---
+++ app/price/price.service.ts
@@ -0,0 +1,3 @@
+export class PriceService {
+    price = [1,2,3,4,5];
+}
