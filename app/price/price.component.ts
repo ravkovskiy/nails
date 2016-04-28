32108ac0f36ca51def07667c2c921fe9e7310d66
ravkovskiy <kos1992@mail.ru>
2016-04-29 00:43:08 +0300

    add bootstrap's price-list whith type 'accordeon'

--- app/price/price.component.ts
+++ app/price/price.component.ts
@@ -2,14 +2,7 @@ import {Component} from "angular2/core";
 import {PriceService} from "../price/price.service";
 @Component({
     selector: 'price_component',
-    template: `
-        <div class='price'>
-            <span>Price</span>
-            <li *ngFor="#price of priceService.price">
-                {{price}}
-            </li>
-        </div>
-    `
+    templateUrl: "app/price/price.template.html",
 })
 export class PriceComponent{
     constructor(public priceService:PriceService) {
--- app/price/price.service.ts
+++ app/price/price.service.ts
@@ -1,3 +1,44 @@
 export class PriceService {
-    price = [1,2,3,4,5];
+    priceList = [
+        {
+        title: 'Маникюр (аппаратный, комбинированный, без последующего покрытия гель-лаком)',
+        price: '12 000 руб.',
+        href: 'collapseOne',
+        },
+        {
+        title: 'SPA-маникюр',
+        price: '32 000 руб.',
+        href: 'collapseTwo', 
+        },
+        {
+        title: 'Мужской маникюр',
+        price: '32 000 руб.',
+        href: 'collapseThree', 
+        },
+        {
+        title: 'Запечатывание ногтей',
+        price: '12 000 руб.',
+        href: 'collapseFour', 
+        },
+        {
+        title: 'Долговременное покрытие гель-лаком (однотонное, разные цвета, без маникюра)',
+        price: '12 000 руб.',
+        href: 'collapseFive', 
+        },
+        {
+        title: 'Дизайны: Френч, Лунный, Ruffian, Halo Nails',
+        price: '12 000 руб.',
+        href: 'collapseSix', 
+        },
+        {
+        title: 'Дизайны: Двойной Ruffian, Лунный + Френч',
+        price: '12 000 руб.',
+        href: 'collapseSeven', 
+        },
+        {
+        title: 'Дизайн с помощью блёсток, пигментов, слюды, шестигранников, ромбиков, ленты',
+        price: '12 000 руб.',
+        href: 'collapseEight', 
+        }
+    ];
 }
