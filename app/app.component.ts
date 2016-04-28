a01cfbb5437e59539844d0b76f16177b9d96a1f2
ravkovskiy <kos1992@mail.ru>
2016-04-28 15:15:09 +0300

    add footer from footer_component in each page

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,15 @@ import {ActionComponent} from './action/action.component';
 import {PortfolioComponent} from './portfolio/portfolio.component';
 import {ReviewsComponent} from './reviews/reviews.component';
 import {ContactsComponent} from './contacts/contacts.component';
+import {InputUserComponent} from './inputUser/inputUser.component';
+import {FooterComponent} from './footer/footer.component';
 import {ROUTER_DIRECTIVES} from "angular2/router";
 import {RouteConfig} from "angular2/router";
     
 
 @Component({
     selector: 'my-app',
-    directives: [CarouselComponent, PriceComponent, ActionComponent, PortfolioComponent, ReviewsComponent, ContactsComponent, ROUTER_DIRECTIVES],
+    directives: [CarouselComponent, PriceComponent, ActionComponent, PortfolioComponent, ReviewsComponent, ContactsComponent, InputUserComponent, FooterComponent, ROUTER_DIRECTIVES],
     templateUrl:"./app/header/header.template.html", 
 })
 @RouteConfig([
@@ -21,5 +23,6 @@ import {RouteConfig} from "angular2/router";
     {path: '/portfolio', name: 'Portfolio', component: PortfolioComponent},
     {path: '/reviews', name: 'Reviews', component: ReviewsComponent},
     {path: '/contacts', name: 'Contacts', component: ContactsComponent},
+    {path: '/input', name: 'InputUser', component: InputUserComponent},
 ])
 export class AppComponent { }
---
+++ app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "angular2/core";
+@Component({
+    selector: 'footer_component',
+    template: `
+        <footer>by Konstantin Ravkovskiy</footer>
+    `
+})
+export class FooterComponent{}
