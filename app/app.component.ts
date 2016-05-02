cd812ef9126f6f1924ecc91c580e6f7eccdc0c45
ravkovskiy <kos1992@mail.ru>
2016-05-02 23:23:03 +0300

    add main.component instead carousel.component add carousel.component in main.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from 'angular2/core';
-import {CarouselComponent} from './carousel/carousel.component';
 import {PriceComponent} from './price/price.component';
 import {ActionComponent} from './action/action.component';
 import {PortfolioComponent} from './portfolio/portfolio.component';
@@ -7,17 +6,18 @@ import {ReviewsComponent} from './reviews/reviews.component';
 import {ContactsComponent} from './contacts/contacts.component';
 import {InputUserComponent} from './inputUser/inputUser.component';
 import {FooterComponent} from './footer/footer.component';
+import {MainComponent} from './main/main.component';
 import {ROUTER_DIRECTIVES} from "angular2/router";
 import {RouteConfig} from "angular2/router";
     
 
 @Component({
     selector: 'my-app',
-    directives: [CarouselComponent, PriceComponent, ActionComponent, PortfolioComponent, ReviewsComponent, ContactsComponent, InputUserComponent, FooterComponent, ROUTER_DIRECTIVES],
+    directives: [MainComponent, PriceComponent, ActionComponent, PortfolioComponent, ReviewsComponent, ContactsComponent, InputUserComponent, FooterComponent, ROUTER_DIRECTIVES],
     templateUrl:"./app/header/header.template.html", 
 })
 @RouteConfig([
-    {path: '/carousel', name: 'Carousel', component: CarouselComponent, useAsDefault: true},
+    {path: '/main', name: 'Main', component: MainComponent, useAsDefault: true},
     {path: '/price', name: 'Price', component: PriceComponent},
     {path: '/action', name: 'Action', component: ActionComponent},
     {path: '/portfolio', name: 'Portfolio', component: PortfolioComponent},
