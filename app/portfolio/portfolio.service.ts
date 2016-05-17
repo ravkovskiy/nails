7c58b22b5a98bbe3c4df4d0cf7d783b99f9a552d
ravkovskiy <kos1992@mail.ru>
2016-05-17 18:19:23 +0300

    start creating sorting for portfolio from date

--- app/portfolio/portfolio.service.ts
+++ app/portfolio/portfolio.service.ts
@@ -3,50 +3,167 @@ export class PortfolioService {
         {
             image: 'kot1.jpeg',
             alt: '...',
+            month: 5,
+            year: 2016,
         },
         {
             image: 'kot2.jpeg',
             alt: '...',
+            month: 5,
+            year: 2016,
         },
         {
             image: 'kot3.jpg',
             alt: '...',
+            month: 5,
+            year: 2016,
         },
         {
             image: 'kot1.jpeg',
             alt: '...', 
+            month: 5,
+            year: 2016,
         },
         {
             image: 'kot3.jpg',
             alt: '...',
+            month: 4,
+            year: 2016,
         },
         {
             image: 'kot2.jpeg',
             alt: '...', 
+            month: 4,
+            year: 2016,
         },
         {
             image: 'kot3.jpg',
             alt: '...', 
+            month: 3,
+            year: 2016,
         },
         {
             image: 'kot2.jpeg',
             alt: '...',
+            month: 3,
+            year: 2016,
         },
         {
             image: 'kot1.jpeg',
             alt: '...',
+            month: 3,
+            year: 2016,
         },
         {
             image: 'kot2.jpeg',
             alt: '...',
+            month: 7,
+            year: 2015,
         },
         {
             image: 'kot3.jpg',
             alt: '...',
+            month: 7,
+            year: 2015,
         },
         {
             image: 'kot2.jpeg',
             alt: '...', 
+            month: 5,
+            year: 2015,
         }
+    ];
+    portfolioCatalog = [
+        {
+            year: 2016,
+            months: [
+                            [
+                                {
+                                    image: 'kot1.jpeg',
+                                    alt: '...',
+                                    month: 5,
+                                    year: 2016,
+                                },
+                                {
+                                    image: 'kot2.jpeg',
+                                    alt: '...',
+                                    month: 5,
+                                    year: 2016,
+                                },
+                                {
+                                    image: 'kot3.jpg',
+                                    alt: '...',
+                                    month: 5,
+                                    year: 2016,
+                                },
+                                {
+                                    image: 'kot1.jpeg',
+                                    alt: '...', 
+                                    month: 5,
+                                    year: 2016,
+                                },
+                            ],
+                            [
+                                {
+                                        image: 'kot3.jpg',
+                                        alt: '...',
+                                        month: 4,
+                                        year: 2016,
+                                    },
+                                    {
+                                        image: 'kot2.jpeg',
+                                        alt: '...', 
+                                        month: 4,
+                                        year: 2016,
+                                    },
+                            ],
+                            [
+                                {
+                                        image: 'kot3.jpg',
+                                        alt: '...', 
+                                        month: 3,
+                                        year: 2016,
+                                    },
+                                    {
+                                        image: 'kot2.jpeg',
+                                        alt: '...',
+                                        month: 3,
+                                        year: 2016,
+                                    },
+                                    {
+                                        image: 'kot1.jpeg',
+                                        alt: '...',
+                                        month: 3,
+                                        year: 2016,
+                                    },
+                            ],    
+            ],
+        },
+        
+        
+        {
+            year: 2015,
+            months: [
+                [
+                    {
+                        image: 'kot2.jpeg',
+                        alt: '...',
+                        month: 7,
+                    },
+                    {
+                        image: 'kot3.jpg',
+                        alt: '...',
+                        month: 7,
+                    },
+                ],
+                [
+                    {
+                        image: 'kot2.jpeg',
+                        alt: '...', 
+                        month: 5,
+                    }
+                ],
+            ],             
+        },
     ];
 }
