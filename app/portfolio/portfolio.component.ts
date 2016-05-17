2bf9ea3d7697b78da4110b005ba164228f3e05dc
ravkovskiy <kos1992@mail.ru>
2016-05-17 12:56:04 +0300

    add closing modal-window, events change from 'click' on 'mousedown'

--- app/portfolio/portfolio.component.ts
+++ app/portfolio/portfolio.component.ts
@@ -6,32 +6,32 @@ import {PortfolioService} from "../portfolio/portfolio.service";
 })
 export class PortfolioComponent{
     constructor(public portfolioService:PortfolioService) {
+        
     }
     //need change start value portfolioItem!!!!!!!!!!!
     portfolioItem = this.portfolioService.portfolioList[0];
     onClickPortfolioItem(clickItem) {
         this.portfolioItem=clickItem;
+        var closeModal=document.getElementsByClassName('carousel-control')[1];
+        var innerWidthBrowser = window.innerWidth;
+        var clientWidthBrowser = document.documentElement.clientWidth;
+        closeModal.style.width=clientWidthBrowser*0.15+(innerWidthBrowser-clientWidthBrowser)+'px';
     }
-    onClickPrevPortfolioItem() {
+    onClickModalPortfolioItem(action) {
         var lengthPortfolio = this.portfolioService.portfolioList.length;
         var positionPortfolioItemClick = this.portfolioService.portfolioList.indexOf(this.portfolioItem);
-        switch(positionPortfolioItemClick) {
-            case 0:
-                this.portfolioItem = this.portfolioService.portfolioList[lengthPortfolio - 1];
+        switch(action) {
+            case 'Next':
+                positionPortfolioItemClick == lengthPortfolio-1 ?
+                    this.portfolioItem = this.portfolioService.portfolioList[0] :
+                    this.portfolioItem = this.portfolioService.portfolioList[positionPortfolioItemClick + 1];
                 break;
-            default: 
-                this.portfolioItem = this.portfolioService.portfolioList[positionPortfolioItemClick - 1];
-        }
-    }
-    onClickNextPortfolioItem() {
-        var lengthPortfolio = this.portfolioService.portfolioList.length;
-        var positionPortfolioItemClick = this.portfolioService.portfolioList.indexOf(this.portfolioItem);
-        switch(positionPortfolioItemClick) {
-            case lengthPortfolio - 1:
-                this.portfolioItem = this.portfolioService.portfolioList[0];
+            case 'Prev':
+                positionPortfolioItemClick == 0 ?
+                    this.portfolioItem = this.portfolioService.portfolioList[lengthPortfolio - 1] :
+                    this.portfolioItem = this.portfolioService.portfolioList[positionPortfolioItemClick - 1];
                 break;
-            default: 
-                this.portfolioItem = this.portfolioService.portfolioList[positionPortfolioItemClick + 1];
         }
+      
     }
 }
